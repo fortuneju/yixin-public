8543e08136f9e5632c3e7b320f5c90cc97f39d29
zfj0952 <zfj0952@users.noreply.github.com>
2014-08-30 11:53:15 +0800

    Update yixin.php

--- yixin.php
+++ yixin.php
@@ -6,6 +6,9 @@ $echostr    =$_GET["echostr"];//随机字符串//将token、timestamp、nonce三
 $signarray =array("zfjaxj515618",$timestamp,$nonce);//将三个参数生成数组
 sort($signarray);//参数数组按字典排序//将数组中三个参数字符串拼接成一个字符串，并进行sha1加密
 $signstr =sha1(implode("",$signarray));//将开发者获得加密后的字符串可与signature对比，标识该请求来源于易信
-if($signstr ==$signature){echo $echostr;//确认此次GET请求来自易信服务器，返回echostr参数内容}
+if($signstr ==$signature)
+{
+  echo $echostr;//确认此次GET请求来自易信服务器，返回echostr参数内容
+}
 
 ?>
